df84ea736797dce54d18e9d6fcdd88c198b0e928
John Matthews <jmatthews@redhat.com>
2013-01-24 20:01:19 -0500

    Fixed bash script to test out create of link through REST API

--- playpen/curl_scripts/create_link.sh
+++ playpen/curl_scripts/create_link.sh
@@ -1,17 +1,13 @@
 ADDR="127.0.0.1"
 PORT=9000
 TIMESTAMP=`date +"%m-%d-%y_%H-%M-%S"`
-POST_DATA="{\"url\":\"http://example.com/${TIMESTAMP}\", \"description\":\"Example description\"}"
 echo "Input:"
 echo ${POST_DATA} | python -mjson.tool
-echo "Received:"
-DATA=`curl -s -X POST --data ${POST_DATA} ${ADDR}:${PORT}/api/link`
-if [ $? -eq 0 ]
-then
-  echo ${DATA} | python -mjson.tool
-else
-  echo ${DATA}
-fi
 
+echo "Curl Command:"
+echo curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json"  -d "{\"url\":\"http://example.com/${TIMESTAMP}\", \"description\":\"Example description\"}" ${ADDR}:${PORT}/api/link
+
+echo "Received:"
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json"  -d "{\"url\":\"http://example.com/${TIMESTAMP}\", \"description\":\"Example description\"}" ${ADDR}:${PORT}/api/link
 echo ""
 
