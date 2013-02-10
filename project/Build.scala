0fe09ff23c51b23f918f3ec1f1f10836f4f06c05
John Matthews <jwmatthews@gmail.com>
2013-02-10 12:04:24 -0500

    Removed usage of webjars, js/css now fetched from js cdn

--- project/Build.scala
+++ project/Build.scala
@@ -9,16 +9,14 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     "org.mongodb" %% "casbah-core" % "2.5.0",
-    "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT",
-    "com.github.twitter" % "bootstrap" % "2.0.2"
+    "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
 
     resolvers ++= Seq(
       "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases",
-      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-      "webjars" at "http://webjars.github.com/m2"
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     )
   )
 
