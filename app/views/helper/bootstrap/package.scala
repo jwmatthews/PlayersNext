214d5fa8a0226535351d12ae62a5e3e2ac0de3a5
John Matthews <jwmatthews@gmail.com>
2013-03-17 11:28:05 -0400

    Moving to ember.js for main view, removing unused server side generated html

--- app/views/helper/bootstrap/package.scala
+++
@@ -1,7 +0,0 @@
-package views.html.helper
-
-package object bootstrap {
-  implicit val fieldConstructor = new FieldConstructor {
-    def apply(elements: FieldElements) = bootstrap.bootstrapFieldConstructor(elements)
-  } 
-}
--- public/js/app/templates.js
+++ public/js/app/templates.js
@@ -1,3 +1,56 @@
+Ember.TEMPLATES['application'] = Ember.Handlebars.compile('' +
+    '<div class="masthead">' +
+        '<h3 class="muted">PlayersNext</h3>' +
+        '<div class="navbar">' +
+            '<div class="navbar-inner">' +
+                '<div class="container">' +
+                    '<ul class="nav">' +
+                      '<li>{{#linkTo "landing"}}Home{{/linkTo}}</li>' +
+                      '<li>{{#linkTo "link.index"}}Articles{{/linkTo}}</li>' +
+                      '<li>{{#linkTo "link.index"}}Guidance{{/linkTo}}</li>' +
+                      '<li>{{#linkTo "link.index"}}Sports Gear{{/linkTo}}</li>' +
+                      '<li>{{#linkTo "link.index"}}Find Coaches/Programs{{/linkTo}}</li>' +
+                      '<li>{{#linkTo "link.index"}}Community{{/linkTo}}</li>' +
+                    '</ul>' +
+                '</div>' +
+            '</div>' +
+        '</div><!-- /.navbar -->' +
+      '</div>' +
+      '<div class="container">{{outlet}}</div>' 
+);
+
+
+Ember.TEMPLATES['landing'] = Ember.Handlebars.compile('' +
+    '<!-- Jumbotron -->' +
+    '<div class="jumbotron">' +
+        '<h1>Marketing Stuff!</h1>' +
+        '<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>' +
+        '<a class="btn btn-large btn-success" href="#">Get started today</a>' +
+    '</div>' +
+
+    '<hr>' +
+    '<!-- Example row of columns -->' +
+    '<div class="row-fluid">' +
+        '<div class="span4">' +
+            '<h2>Discover how to Play</h2>' +
+            '<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>' +
+            '<p><a class="btn" href="#">View details &raquo;</a></p>' +
+        '</div>' +
+        '<div class="span4">' +
+            '<h2>Learn to Compete</h2>' +
+            '<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>' +
+            '<p><a class="btn" href="#">View details &raquo;</a></p>' +
+        '</div>' +
+        '<div class="span4">' +
+            '<h2>Focus on Winning</h2>' +
+            '<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>' +
+            '<p><a class="btn" href="#">View details &raquo;</a></p>' +
+        '</div>' +
+    '</div>'
+);
+
+
+
 Ember.TEMPLATES['link/index'] = Ember.Handlebars.compile('' +
     '{{view Ember.TextField placeholder="url" valueBinding="view.url"}}' +
     '{{view Ember.TextField placeholder="title" valueBinding="view.title"}}' +
